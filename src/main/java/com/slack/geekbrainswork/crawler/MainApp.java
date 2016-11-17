ed2cae81c1e4e486177f878ae2a8e1ad22dc67db
monosoul <Kloz.Klaud@gmail.com>
2016-11-17 21:39:02 +0500

    MainApp template

--- src/main/java/com/slack/geekbrainswork/crawler/MainApp.java
+++ src/main/java/com/slack/geekbrainswork/crawler/MainApp.java
@@ -1,7 +1,28 @@
 package com.slack.geekbrainswork.crawler;
 
+import com.slack.geekbrainswork.crawler.model.*;
+import com.slack.geekbrainswork.crawler.util.DAOFactory;
+import com.slack.geekbrainswork.crawler.util.SFactory;
+import org.hibernate.Hibernate;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Calendar;
+import java.util.List;
+
 /**
  * Created by Andrey on 09.11.2016.
  */
 public class MainApp {
+    public static void main(String[] args) {
+        try{
+            
+        }
+        finally {
+            // закрываем сессию с БД
+            SFactory.get().getCurrentSession().close();
+            SFactory.get().close();
+        }
+    }
 }
